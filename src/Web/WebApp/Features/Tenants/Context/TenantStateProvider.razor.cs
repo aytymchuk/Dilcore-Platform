5dde7414079353aad43539344ba7ae15e6fb4e10
Arsen Tymchuk <a.y.tymchuk@gmail.com>
2026-02-12 01:05:48 +0100

    refactor: remove SystemName parameter from GetTenantBySystemNameQuery invocation.

--- src/Web/WebApp/Features/Tenants/Context/TenantStateProvider.razor.cs
+++ src/Web/WebApp/Features/Tenants/Context/TenantStateProvider.razor.cs
@@ -41,7 +41,7 @@ public partial class TenantStateProvider : ComponentBase
             return;
         }
 
-        var result = await Sender.Send(new GetTenantBySystemNameQuery(SystemName));
+        var result = await Sender.Send(new GetTenantBySystemNameQuery());
 
         if (result.IsFailed)
         {
